5201c077104b976b28376601839c0048a6d2e2ed
JackTsang <jack_zjz@foxmail.com>
2014-05-26 15:58:48 +0800

    相册

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -8,17 +8,27 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+                   ../../Classes/HelloWorldScene.cpp \
+				   ../../Classes/GameScene.cpp \
+				   ../../Classes/Constants.cpp \
+				   ../../Classes/MainLayer.cpp \
+				   ../../Classes/PhotoLayer.cpp \
+				   ../../Classes/PhotoMenu.cpp \
+				   ../../Classes/StartLayer.cpp \
+				   ../../Classes/VidioLayer.cpp 
+				   
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
 LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocostudio_static
 
 
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,2d)
 $(call import-module,audio/android)
+$(call import-module,editor-support/cocostudio)
 $(call import-module,Box2D)
